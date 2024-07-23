d415391d5faa8e4a50c8e6223e1197700284afc2
Kamil <kamilwro1998@gmail.com>
2024-07-23 23:03:54 +0200

    feat: new setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo -e "\nStarting setup...\n"
+
+# Install node packages in the root folder
+echo -e "\nInstalling packages in the root folder...\n"
+npm install
+
+# Install node packages in the frontend folder
+cd frontend || exit
+echo -e "\nInstalling packages in the frontend folder...\n"
+npm install
+
+cd ..
+
+echo -e "\nBuilding application...\n"
+npm run build
+
+echo -e "\n\033[0;32mSuccess! Serve the project by running \033[0;35mnpm start\033[0m"
